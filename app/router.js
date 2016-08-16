45dd4ece31aeafd52e63667c1d7d61df2c7b34b8
austin <austin.cawley@gmail.com>
2016-08-16 19:42:41 -0400

    Mapped out the router and continued sign on configuration

--- app/router.js
+++ app/router.js
@@ -6,7 +6,24 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('dashboard');
+  /* Default page routes */
+  // Splash page
+  this.route('index', { path: '/' });
+  // 404
+  this.route('page-not-found', { path: '*wildcard' });
+
+  /* Authenticated routes */
+  // 'Portal' lolol
+  this.authenticatedRoute('dashboard');
+  // Document Routes
+  this.authenticatedRoute('document', { path: 'document/:id' }, function() {
+    // Viewing should be the default mode of a document
+    this.authenticatedRoute('view', { path: '/'});
+    this.authenticatedRoute('view', { path: '*wildcard'});
+    this.authenticatedRoute('edit', { path: 'edit'});
+    this.authenticatedRoute('create', { path: 'create'});
+
+  });
 });
 
 export default Router;
--- app/routes/application.js
+++ app/routes/application.js
@@ -3,18 +3,36 @@
  */
 import Ember from 'ember';
 export default Ember.Route.extend({
+    /**
+     * Check to see if the user's session is present.
+     * @return {*|Promise|Promise.<T>}
+     */
     beforeModel: function() {
-        return this.get('session').fetch().catch(function() {
-        });
+        return this.get('session').fetch().catch(function() {});
     },
     actions: {
+        /**
+         * If the user is not logged in, reroute them to the home page.
+         */
+        accessDenied: function() {
+          this.transitionTo('index')
+        },
+        /**
+         * The default page for logged in users is their 'dashboard'
+         * For now, if a PI signs in we will reroute them from there.
+         * @param {string} provider
+         */
         signIn: function(provider) {
+            const route = this;
             this.get('session').open('firebase', {
                 provider: provider
             }).then(function(data) {
-                console.log(data.currentUser);
+                route.transitionTo('dashboard');
             });
         },
+        /**
+         * Sign the user out of the session.
+         */
         signOut: function() {
             this.get('session').close();
         }
