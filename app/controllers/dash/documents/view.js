eb003fc9a6a9132843825f59efb502606926fb59
austin <austin.cawley@gmail.com>
2016-08-25 17:38:48 -0400

    Working on the view actions

--- app/controllers/dash/documents/view.js
+++ app/controllers/dash/documents/view.js
@@ -4,15 +4,23 @@ export default Ember.Controller.extend({
   mailer: Ember.inject.service(),
   showSendToAdminDialog: false,
   actions: {
+    /**
+     * Redirect back to the create page
+     */
+    makeChanges() {
+      this.transitionToRoute('dash.documents.create', this.get('model.id'));
+    },
     openSendToAdminDialog() {
       this.set('showSendToAdminDialog', true);
     },
     closeSendToAdminDialog(action) {
       switch(action) {
         case 'cancel':
+          this.set('showSendToAdminDialog', false);
           break;
         case 'confirm':
-          // Send to admin
+          // Send to admin using the mailer
+          this.set('showSendToAdminDialog', false);
           break;
       }
     }
--- app/routes/dash/documents/edit.js
+++ app/routes/dash/documents/edit.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  titleToken: 'Edit'
+  titleToken: 'Edit',
+  setupController(controller, model){
+    this._super(controller, model);
+    // Bind the currentModel to the controller so we can access it there
+    controller.set('currentModel', model);
+  },
 });
